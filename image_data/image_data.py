bc694d366b50dbc3b7bf918d555be338181f9234
Radek Starosta <radek.starosta@gmail.com>
2017-06-27 13:09:51 +0200

    Upload and show an image.

--- image_data/image_data.py
+++ image_data/image_data.py
@@ -1,9 +1,42 @@
-from flask import Flask
+import io
+from flask import Flask, request, redirect, url_for, flash, send_file
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World"
 
-if __name__=="__main__":
+def allowed_file(filename):
+  return '.' in filename and \
+         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'file' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      return send_file(io.BytesIO(file.read()), attachment_filename=file.filename)
+  return '''
+      <!doctype html>
+      <title>Upload new File</title>
+      <h1>Upload new File</h1>
+      <form method=post enctype=multipart/form-data>
+        <p><input type=file name=file>
+           <input type=submit value=Upload>
+      </form>
+      '''
+
+
+if __name__ == "__main__":
   app.run()
