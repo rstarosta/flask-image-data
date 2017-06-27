e2512a5c55b60caacf1ee125296ba74c971b0cc8
Radek Starosta <radek.starosta@gmail.com>
2017-06-27 13:11:28 +0200

    Reformat indentation to PEP8.

--- image_data/image_data.py
+++ image_data/image_data.py
@@ -8,26 +8,26 @@ app = Flask(__name__)
 
 
 def allowed_file(filename):
-  return '.' in filename and \
-         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-  if request.method == 'POST':
-    # check if the post request has the file part
-    if 'file' not in request.files:
-      flash('No file part')
-      return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit a empty part without filename
-    if file.filename == '':
-      flash('No selected file')
-      return redirect(request.url)
-    if file and allowed_file(file.filename):
-      return send_file(io.BytesIO(file.read()), attachment_filename=file.filename)
-  return '''
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            return send_file(io.BytesIO(file.read()), attachment_filename=file.filename)
+    return '''
       <!doctype html>
       <title>Upload new File</title>
       <h1>Upload new File</h1>
@@ -39,4 +39,4 @@ def upload_file():
 
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
